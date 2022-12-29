5820cbaca13f73b868c19da2c79c41e6c0496208
thiên lê <109102523+tlee2210@users.noreply.github.com>
2022-12-29 21:31:18 +0700

    cnhap

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,14 @@ angular.module("myApp.menu",[])
     .controller("menuCtrl", ["$scope", "movieAPIservice", function($scope, movieAPIservice){
         
         movieAPIservice.getBrnads().then(function(reponse){
-            $scope.brandslist = reponse.data;
-            console.log(reponse.data);
+            var data = reponse.data;
+            $scope.brandslist = data;
+            console.log($scope.brandslist);
         })
 
         movieAPIservice.getAccessories().then(function(reponse){
-            $scope.access = reponse.data;
-            console.log(reponse.data);
+            var data = reponse.data
+            $scope.access = data;
+            console.log($scope.access);
         })
     }])
