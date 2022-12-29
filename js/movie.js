8975f9ba3301d8b7e444930c9011428b4c791563
thiên lê <109102523+tlee2210@users.noreply.github.com>
2022-12-29 09:57:38 +0700

    Update movie.js

--- js/movie.js
+++ js/movie.js
@@ -3,14 +3,11 @@ angular.module("myApp.movie", [])
 
         $scope.movieId = $routeParams.Id
 
-        movieAPIservice.getHome().then(function(reponse){
+        movieAPIservice.getProduct().then(function(reponse){
             var data = reponse.data;
             $scope.movie = $filter("filter")(data, {Id: parseInt($scope.movieId)}, true)[0];
             console.log($scope.movie)
         })
 
-        movieAPIservice.getProduct().then(function(reponse){
-            $scope.dataMain = reponse.data;
-            console.log(reponse.data);
-        })       
+        
     }])
