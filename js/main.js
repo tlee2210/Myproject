e25f7438b8e24cbe9a74994024db39ce533e5e5b
thiên lê <109102523+tlee2210@users.noreply.github.com>
2022-12-31 19:17:15 +0700

    cnhap

--- js/main.js
+++ js/main.js
@@ -40,6 +40,16 @@
 
 ***************************************************************/
 
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+	counter = 1;
+  }
+}, 3000);
+// ======================================================================
+
 
 (function($){
     'use strict';
