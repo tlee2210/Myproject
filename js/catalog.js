9f718d12d93047153a536fc9a3c0da7d45d89fef
thiên lê <109102523+tlee2210@users.noreply.github.com>
2023-01-01 00:50:07 +0700

    cnhap

--- js/catalog.js
+++ js/catalog.js
@@ -4,14 +4,14 @@ angular.module("myApp.genres",["ui.bootstrap"])
         $scope.currentPage = 1;
         $scope.pageSize=6;
 
-        $scope.catalogList = $routeParams.genresName
-        console.log($scope.catalogList);
+        $scope.genresName = $routeParams.genresName
+        console.log($scope.genresName);
 
 
         productAPIservice.getProduct().then(function(reponse){
             var data = reponse.data;
             $scope.catalogList = $filter("filter")(data, function(product){
-                return $filter("filter")(product.Type, {Name: $scope.catalogList}).length;
+                return $filter("filter")(product.Type, {Name: $scope.genresName}).length;
             })
             // console.log($scope.catalogList);
         })
@@ -20,5 +20,6 @@ angular.module("myApp.genres",["ui.bootstrap"])
             $scope.productList = data;
             // console.log($scope.productList);
         })
+        
     }])
 
