d6650867f32fb0143d1e0b3136eda8dfc3133ad1
thiên lê <109102523+tlee2210@users.noreply.github.com>
2022-12-31 19:19:18 +0700

    cnhap

--- js/main.js
+++ js/main.js
@@ -40,14 +40,14 @@
 
 ***************************************************************/
 
-var counter = 1;
-setInterval(function(){
-  document.getElementById('radio' + counter).checked = true;
-  counter++;
-  if(counter > 4){
-	counter = 1;
-  }
-}, 3000);
+// var counter = 1;
+// setInterval(function(){
+//   document.getElementById('radio' + counter).checked = true;
+//   counter++;
+//   if(counter > 4){
+// 	counter = 1;
+//   }
+// }, 3000);
 // ======================================================================
 
 
