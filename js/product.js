a667ac973faa7ea6b7e8f1da6d528426e3516fa4
thiên lê <109102523+tlee2210@users.noreply.github.com>
2023-01-03 02:11:26 +0700

    cnhap

--- js/product.js
+++ js/product.js
@@ -1,4 +1,4 @@
-angular.module("myApp.product", [])
+angular.module("myApp.product", ["ui.bootstrap"])
     .controller("productCtrl", ["$scope","$filter", "productAPIservice","$routeParams", function($scope, $filter, productAPIservice, $routeParams){
 
         $scope.productId = $routeParams.Id
@@ -8,4 +8,10 @@ angular.module("myApp.product", [])
             $scope.product = $filter("filter")(data, {Id: parseInt($scope.productId)}, true)[0];
             console.log($scope.product)
         })
+
+        $scope.carts=[];
+        $scope.addToCart = function(product){
+            $scope.carts.push(product)
+            console.log($scope.carts)
+        }
     }])
