628439cab2dab008c92c470ac3ac73e2e4f1d7c9
thiên lê <109102523+tlee2210@users.noreply.github.com>
2023-01-04 20:32:50 +0700

    CNHAP

--- js/app.js
+++ js/app.js
@@ -20,13 +20,13 @@ var app = angular.module("myApp", [
 app.config(function($routeProvider){
     $routeProvider
     .when("/", {templateUrl : "home.html", controller:"homeCtrl"})
-    // .when("/myAccount", {templateUrl : "myAccount.html", controller:"myAccount"})
+    .when("/myAccount", {templateUrl : "myAccount.html", controller:"myAccount"})
     .when("/cart", {templateUrl : "cart.html", controller:"cartCtrl"})
     // .when("/checkout", {templateUrl : "checkout.html", controller:"checkout"})
     // .when("/catalog", {templateUrl : "catalog.html", controller:"catalogCtrl"})
-    // .when("/contact", {templateUrl : "contact.html", controller:"contact"})
-    // .when("/aboutus", {templateUrl : "aboutus.html", controller:"aboutus"})
-    // .when("/feedback", {templateUrl : "feedback.html", controller:"feedback"})
+    .when("/contact", {templateUrl : "contact.html", controller:"contact"})
+    .when("/aboutus", {templateUrl : "aboutus.html", controller:"aboutus"})
+    .when("/feedback", {templateUrl : "feedback.html", controller:"feedback"})
     .when("/product/:Id", {templateUrl : "product.html", controller:"productCtrl"})
     .when("/genres/:genresName", {templateUrl : "catalog.html", controller:"genresCtrl"})
     // .when("/Brands/:genresBrand", {templateUrl : "catalog.html", controller:"menuCtrl"})
